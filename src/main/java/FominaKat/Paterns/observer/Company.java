1b9b67f3dcf115201327e6db1a779112b0c4aab7
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2024-03-29 18:32:21 +0400

    паттерн наблюдатель

--- src/main/java/FominaKat/Paterns/observer/Company.java
+++ src/main/java/FominaKat/Paterns/observer/Company.java
@@ -1,10 +1,12 @@
 package FominaKat.Paterns.observer;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Company {
 
     private Random random = new Random();
+    private ArrayList<String> staff= new ArrayList<>();
     private String name;
 
     private Publisher jobAgency;
@@ -15,16 +17,25 @@ public class Company {
         this.name = name;
         this.jobAgency = jobAgency;
         this.maxSalary = maxSalary;
+        initStaff();
     }
+
+    private void initStaff(){
+        staff.add("руководитель");
+        staff.add("младший специалист");
+        staff.add("стажер");
+        staff.add("помощник");
+    }
+
 
     public void needEmployee() {
         int salary = random.nextInt(maxSalary);
         //рандомно выберем профессию
         int rnd = random.nextInt(0, Profession.values().length);
         Profession need = Profession.values()[rnd];
-
-        System.out.printf("\nОткрыта вакансия -'%s', в компании %s (з/п = %d)\n", need, this.name, salary);
-        jobAgency.sendOffer(name, new Vacancy(need, need.toString(), salary));
+        String description = String.format("%s %s", staff.get(random.nextInt(staff.size())), need.name());
+        System.out.printf("\nОткрыта вакансия -'%s', в компании %s (з/п = %d)\n", description, this.name, salary);
+        jobAgency.sendOffer(name, new Vacancy(need, description, salary));
 
     }
 }
